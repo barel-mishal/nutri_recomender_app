ee9b29e4fa653f727cb6e7a8e9783825aa94b1c1
barelmishal <dreamwork@dreamwork.network>
2022-02-15 11:59:19 +0200

    add requirements.txt

--- recommender.py
+++ recommender.py
@@ -7,7 +7,8 @@ from recommender_system import content_base_recommender
 __DIRNAME__ = os.path.dirname(os.path.realpath(__file__))
 dataset = pd.read_csv(os.path.join(__DIRNAME__, 'csvs', 'israeli_data.csv'))
 
-st.write('שמות מצרכי המזון במידה', dataset['shmmitzrach'].rename({'shmmitzrach': 'שם מצרך'}))
+st.write('שמות מצרכי המזון במידה', dataset[['shmmitzrach']].rename(columns={'shmmitzrach': 'שם מצרך'}))
+
 
 item = st.text_input('כדי לרשום שם של מוצר מזון כדי לקבל המלצה למשל חסה: ')
 if item != '':
